4e2a0123454e2c84c70f559136f874019a24f9e1
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-11 19:02:31 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+// task 1
+function writeCards(arrayOfNames, event = "birthday") {
+  let arrayOfGreeting = [];
+
+  for (let name of arrayOfNames) {
+    arrayOfGreeting.push(
+      `Thank you, ${name}, for the wonderful ${event} gift!`
+    );
+  }
+
+  return arrayOfGreeting;
+}
+
+let givenNames = ["Charlie", "Samip", "Ali"];
+writeCards(givenNames, "birthday");
+
+// task 2
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
